1e88d9e805078bcbb015bbba80b95a2a874083a5
mczms <mcz.ms@czeremin.de>
2024-04-06 17:34:09 +0200

    add graphic

--- PI analytisch berechnen.py
+++ PI analytisch berechnen.py
@@ -1,22 +1,34 @@
 import random
 import math
+import matplotlib.pyplot as plt
+
 
 
 trycount_str = input("Number of attempts")
 trycount = int(trycount_str)
 
+hitx    = []
+nothitx = []
+
+hity    = []
+nothity = []
+
+
 hit = 0
 nothit = 0
 
 for i in range(trycount):
-    x = random.uniform(0.0, 1.0)
-    y = random.uniform(0.0, 1.0)
-    #print(f"{x}, {y}")
-    # if (x**2 + y**2)**0.5 <= 1:
+    x=random.uniform(0.0, 1.0)
+    y=random.uniform(0.0, 1.0)
+
     if (x * x + y * y) <= 1:
         hit += 1
+        hitx.append(x)
+        hity.append(y)
     else:
         nothit += 1
+        nothitx.append(x)
+        nothity.append(y)
 
 pi = 4*((hit)/(trycount))
 
@@ -31,4 +43,11 @@ print("Pi         =",pi)
 print("Error      =",Error)
 print("deviation  =",Error/math.pi*100,"%")
 
+#graphic
+
+plt.plot(nothitx,nothity,y,color='none',linestyle='dashed',linewidth = 2, marker= 'o', markersize=3,markerfacecolor='red' , markeredgecolor ='red')
+plt.plot(hitx,hity,color='none',linestyle='dashed',linewidth = 2, marker= 'o', markersize=3,markerfacecolor='green' , markeredgecolor ='green')
+
+plt.grid()
+plt.show()
 
