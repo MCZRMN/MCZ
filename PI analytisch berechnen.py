50d1cce4e82b7d048a66fe5984fb8bcd142f7d5f
mczms <mcz.ms@czeremin.de>
2024-04-06 18:00:49 +0200

    fix bug: accidentally added point removed

--- PI analytisch berechnen.py
+++ PI analytisch berechnen.py
@@ -21,10 +21,12 @@ for i in range(trycount):
     x=random.uniform(0.0, 1.0)
     y=random.uniform(0.0, 1.0)
 
-    if (x * x + y * y) <= 1:
+
+    if (x * x + y * y) <= 1.0:
         hit += 1
         hitx.append(x)
         hity.append(y)
+
     else:
         nothit += 1
         nothitx.append(x)
@@ -45,7 +47,7 @@ print("deviation  =",Error/math.pi*100,"%")
 
 #graphic
 
-plt.plot(nothitx,nothity,y,color='none',linestyle='dashed',linewidth = 2, marker= 'o', markersize=3,markerfacecolor='red' , markeredgecolor ='red')
+plt.plot(nothitx,nothity,color='none',linestyle='dashed',linewidth = 2, marker= 'o', markersize=3,markerfacecolor='red' , markeredgecolor ='red')
 plt.plot(hitx,hity,color='none',linestyle='dashed',linewidth = 2, marker= 'o', markersize=3,markerfacecolor='green' , markeredgecolor ='green')
 
 plt.grid()
