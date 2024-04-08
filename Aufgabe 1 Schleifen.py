67a3bd07571dcefab694cef36b79ebedb8ec9f81
MCZRMN <101340254+MCZRMN@users.noreply.github.com>
2024-04-08 16:03:32 +0200

    Add files via upload

--- Aufgabe 1 Schleifen.py
+++ Aufgabe 1 Schleifen.py
@@ -3,19 +3,19 @@
 
 V1 = 0
 
-for i2 in range(50):
-    V1 += 1
+for i2 in range(25):
+    V1 += 2
     print(V1)
 
 
 V2 = 0
 
 while V2 != 50:
-    V2 += 1
+    V2 += 2
     print(V2)
 
 
 V3 = 0
-for i2 in range(10):
-    print(V3+1,V3+2,V3+3,V3+4,V3+5)
-    V3 += 5
+for i2 in range(5):
+    print(V3+2,V3+4,V3+6,V3+8,V3+10)
+    V3 += 10
