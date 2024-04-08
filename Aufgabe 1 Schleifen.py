6c679ef73e5ed304de6151950c9f6a0879c123c0
MCZRMN <101340254+MCZRMN@users.noreply.github.com>
2024-04-08 15:52:20 +0200

    Add files via upload

---
+++ Aufgabe 1 Schleifen.py
@@ -0,0 +1,21 @@
+# Aufgabe 1
+
+
+V1 = 0
+
+for i2 in range(50):
+    V1 += 1
+    print(V1)
+
+
+V2 = 0
+
+while V2 != 50:
+    V2 += 1
+    print(V2)
+
+
+V3 = 0
+for i2 in range(10):
+    print(V3+1,V3+2,V3+3,V3+4,V3+5)
+    V3 += 5
