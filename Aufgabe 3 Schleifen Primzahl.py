db62b6b4373b2f6b2ee3427d3f48900a75ab8a2b
MCZRMN <101340254+MCZRMN@users.noreply.github.com>
2024-04-11 11:17:40 +0200

    Add files via upload

---
+++ Aufgabe 3 Schleifen Primzahl.py
@@ -0,0 +1,19 @@
+﻿# Aufgabe 3 Schleifen Primzahl
+
+x = int(input("Natürliche Zahl eingeben"))
+print(x)
+
+v1 = x-1
+rest = x % v1
+
+while rest != 0:
+    rest = x % v1
+    v1 -= 1
+
+
+
+if v1 == 0:
+    print("Primzahl")
+
+elif v1 != 1:
+    print("Keine Primzahl")
