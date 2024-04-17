88b852bd91c4d1483a9a7c82857bc3a1195d7652
mczms <mcz.ms@czeremin.de>
2024-04-17 15:20:27 +0200

    Added

--- Dritte Wurzel.py
+++ Dritte Wurzel.py
@@ -1,8 +1,5 @@
-z = int(input("Z EINGEBEN"))
-
-xalt = 10000000
-for i in range(300):
-
-    xneu = (2*xalt + z/xalt**2) / 3
-    xalt = xneu
-print(xneu)
+x = int(input("Zahl"))
+Ergebnis = 0
+for i in range(1,x+1,2):
+    Ergebnis = Ergebnis + i
+print(Ergebnis)
