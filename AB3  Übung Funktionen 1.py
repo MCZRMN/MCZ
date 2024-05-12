7f54f0b4a3f84f6ea7c86a67337493a355a7e33c
mczms <mcz.ms@czeremin.de>
2024-05-12 21:40:35 +0200

    new

---
+++ AB3  Übung Funktionen 1.py
@@ -0,0 +1,21 @@
+# Aufgabe 1
+
+
+V1 = 0
+
+for i2 in range(25):
+    V1 += 2
+    print(V1)
+
+
+V2 = 0
+
+while V2 != 50:
+    V2 += 2
+    print(V2)
+
+
+V3 = 0
+for i2 in range(5):
+    print(V3+2,V3+4,V3+6,V3+8,V3+10)
+    V3 += 10
