4277faee00e3a7ac12afcd2518c02b9f7ff24b36
mczms <mcz.ms@czeremin.de>
2024-05-15 20:22:20 +0200

    added

---
+++ Übungsblatt Funktionen 4    AB 2.py
@@ -0,0 +1,18 @@
+def inteingabe(a, b):
+
+
+    interger = int(input(f"Input Number between {a} and {b}"))
+
+
+    return interger, a > interger or b < interger, a, b
+
+
+
+
+
+
+interger, error, n1, n2 = inteingabe(50, 100)
+
+print("Number is:", interger)
+print(f"The Number should be between {n1} and {n2}")
+print("Error:", error)
